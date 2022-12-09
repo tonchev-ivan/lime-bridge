2cf899b8267ee6a7a8dfd55ec9d641c3509d94ae
Ivan Tonchev <tonchev.b.ivan@gmail.com>
2022-12-09 20:12:05 +0200

    refactoring

--- scripts/bridge-api.js
+++ scripts/bridge-api.js
@@ -6,9 +6,6 @@ const web3Alch = createAlchemyWeb3(
     process.env.ARBITRUM_API_URL_WSS,
 )
 
-const BridgeLocal = require('../artifacts/contracts/Bridge.sol/Bridge.json');
-
-
 const BridgeContract = require('../artifacts/contracts/Bridge.sol/Bridge.json');
 
 const web3 = new Web3("wss://goerli.infura.io/ws/v3/5b37e612a4a9423db4bca98d18b84371");
@@ -18,52 +15,67 @@ const bridgearbitrumSubscriber = new web3Alch.eth.Contract(BridgeContract.abi, p
 
 const bridgearbitrum = new web3arbitrum.eth.Contract(BridgeContract.abi, process.env.ARBITRUM_CONTRACT_ADDRESS, { from: "0x58373aD18bB235d3cD6Ae43D2B922be9f4D43Ca3" });
 const wallet = web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY);
-
+const tokenAddress = [
+    {
+        'name': 'LTT',
+        'network': '5',
+        'address': '0x7aC74cc3eE165B715C190ebcB2a33D429f297614'
+    },
+    {
+        'name': 'LTT',
+        'network': '42161',
+        'address': '0x714dE63FBE27598d042eae13361c09B060969f15'
+    }
+];
 async function main() {
     bridge.events.ClaimTokens(function (error, event) {
         if (error) {
             console.error(error);
         } else {
             console.log(event);
-            minting(event);
+            minting(event, web3, web3arbitrum, bridge, bridgearbitrum, process.env.GOERLI_CONTRACT_ADDRESS, process.env.ARBITRUM_CONTRACT_ADDRESS);
         }
     });
-    bridgearbitrumSubscriber.events.Mint(function (error, event) {
+
+    bridgearbitrumSubscriber.events.ClaimTokens(function (error, event) {
         if (error) {
             console.error(error);
         } else {
-            console.log("TOKEN MINTED");
             console.log(event);
+            minting(event, web3arbitrum, web3, bridgearbitrum, bridge, process.env.ARBITRUM_CONTRACT_ADDRESS, process.env.GOERLI_CONTRACT_ADDRESS);
         }
     });
 }
 
-async function minting(event) {
+async function minting(event, sourceProvider, destinationProvider, sourceContract, destinationContract, sourceContractAddress, destinationContractAddress) {
     try {
-        gasAmount = await bridgearbitrum.methods.mint(process.env.ARBITRUM_TOKEN_CONTRACT_ADDRESS,
+        const destinationProviderId = await destinationProvider.eth.net.getId()
+
+        const tokenName = tokenAddress.filter(function (e) { return e.address == event.returnValues.token })[0].name
+        const destinationTokenAddress = tokenAddress.filter(function (e) { return e.name == tokenName && e.network == destinationProviderId })[0].address
+        gasAmount = await destinationContract.methods.mint(
+            destinationTokenAddress,
             event.returnValues.from,
             event.returnValues.amount,
             event.returnValues.nonce).estimateGas({ from: wallet.address });
-
         const tx = {
             from: wallet.address,
-            to: process.env.ARBITRUM_CONTRACT_ADDRESS,
+            to: destinationContractAddress,
             gas: gasAmount,
             value: 0,
-            data: bridgearbitrum.methods.mint(
-                process.env.ARBITRUM_TOKEN_CONTRACT_ADDRESS,
+            data: destinationContract.methods.mint(
+                destinationTokenAddress,
                 event.returnValues.from,
                 event.returnValues.amount,
                 event.returnValues.nonce
             ).encodeABI()
         };
-        const signPromise = web3arbitrum.eth.accounts.signTransaction(tx, process.env.PRIVATE_KEY);
-
+        const signPromise = destinationProvider.eth.accounts.signTransaction(tx, process.env.PRIVATE_KEY);
         signPromise.then((signedTx) => {
-            const sentTx = web3arbitrum.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);
+            const sentTx = destinationProvider.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);
             sentTx.on("receipt", receipt => {
                 console.log("receipt: " + receipt);
-                confirmClaimEvent(event, bridge, web3, process.env.GOERLI_CONTRACT_ADDRESS);
+                confirmClaimEvent(event, sourceContract, sourceProvider, sourceContractAddress);
             });
             sentTx.on("error", err => {
                 console.log("error: " + err);
@@ -72,7 +84,6 @@ async function minting(event) {
                 console.log("Confirm minting...");
 
             });
-            console.log("test");
         }).catch((err) => {
             console.log("error: " + err);
         });
